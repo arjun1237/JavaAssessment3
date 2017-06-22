0566769b8ad19127dbf3a1e0cd6f6251bb9d1e07
Arjun K Prasad <arjun.blr1237@gmail.com>
2017-06-22 14:20:47 +0100

    Add files via upload

---
+++ products/StockManager.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+
+/**
+ * Manage the stock in a business.
+ * The stock is described by zero or more Products.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StockManager
+{
+    // A list of the products.
+    private ArrayList<Product> stock;
+
+    /**
+     * Initialise the stock manager.
+     */
+    public StockManager()
+    {
+        stock = new ArrayList<>();
+    }
+
+    /**
+     * Add a product to the list.
+     * @param item The item to be added.
+     */
+    public void addProduct(Product item)
+    {
+        stock.add(item);
+    }
+    
+    /**
+     * Receive a delivery of a particular product.
+     * Increase the quantity of the product by the given amount.
+     * @param id The ID of the product.
+     * @param amount The amount to increase the quantity by.
+     */
+    public void delivery(int id, int amount)
+    {
+        // a - temporary name for each element in the stock ArrayList
+        Product a=findProduct(id);
+        if (a!=null){
+            a.increaseQuantity(amount);
+        }
+    }
+    
+    /**
+     * Try to find a product in the stock with the given id.
+     * @return The identified product, or null if there is none
+     *         with a matching ID.
+     */
+    public Product findProduct(int id)
+    {
+        // c - temporary name for each element in the stock ArrayList
+        for(Product c: stock){
+             if(c.getID()==id){
+                 return c;
+             }
+        }
+        return null;
+    }
+    
+    /**
+     * Locate a product with the given ID, and return how
+     * many of this item are in stock. If the ID does not
+     * match any product, return zero.
+     * @param id The ID of the product.
+     * @return The quantity of the given product in stock.
+     */
+    public int numberInStock(int id)
+    {
+        // b - temporary name for each element in the stock ArrayList
+        Product b=findProduct(id);
+        if(b!=null){
+            return b.getQuantity();
+        }
+        return 0;
+    }
+
+    /**
+     * Print details of all the products.
+     */
+    public void printProductDetails()
+    {
+        // x - temporary name for each element in the stock ArrayList
+        for(Product x: stock){
+            System.out.println(x.toString());
+        }
+    }
+}
